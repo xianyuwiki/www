268a375487305b564b368f091f90afbc660ed10a
xianyuwiki <578516121@qq.com>
2016-05-16 21:55:06 +0800

    Update TabIndexHolder.js

--- TabIndexHolder.js
+++ TabIndexHolder.js
@@ -1,4 +1,8 @@
 /*
+作者
+   xianyu.wiki
+说明
+   http://xianyu.wiki/p/TabIndexHolder
 协议
    The MIT License (MIT)
    
@@ -21,10 +25,6 @@
    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
    SOFTWARE.
-作者
-   xianyu.wiki
-说明
-   http://xianyu.wiki/p/TabIndexHolder
 */
 
 ;(function(){
